e017321930664f24f1c2dc23d69dc05b4eff83e1
Marat KH <97257797+Marat1Kh@users.noreply.github.com>
2023-10-30 17:59:07 +0000

    moved files

--- project_1/Program.cs
+++ project_1/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace OpenDotaConsoleApp
+{
+    class Program
+    {
+       public static async Task Main(string[] args)
+        {
+            Console.WriteLine("OpenDota Console App");
+
+            OpenDotaApiClient openDotaApiClient = new OpenDotaApiClient();
+            Console.Write("Enter a match ID: ");
+            if (long.TryParse(Console.ReadLine(), out long matchId))
+            {
+                string matchInfo = await openDotaApiClient.GetMatchInfoAsync(matchId);
+
+                if (!string.IsNullOrEmpty(matchInfo))
+                {
+                    Console.WriteLine("Match Information:");
+                    Console.WriteLine(matchInfo);
+                }
+                else
+                {
+                    Console.WriteLine("Failed to retrieve match information.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid match ID. Please enter a valid numeric match ID.");
+            }
+        }
+    }
+}
